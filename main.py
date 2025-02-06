c276fc2881dc7a85d0df7d98ea6e85b9b2247d40
MCVLIX <mcvlix@eduroam-169-233-222-193.ucsc.edu>
2025-02-06 15:03:10 -0800

    basic pseudo structure

---
+++ main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# Define iniital parameters
+# Initial A, B, P, variances, tmax
+# Define ICs
+
+# Run Simulation
+
+# Plot evolution of A,B,P in t
+# Plot SIR model itself with total infected
+# Plot SIR model with individual infected
---
+++ sir_MEvariants.py
@@ -0,0 +1,1 @@
+# code to run entire simulation
