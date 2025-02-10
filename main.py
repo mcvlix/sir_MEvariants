893a4815ed686c482d7cc8cbfc0732a92db72425
Evan Quintana <etquinta@ucsc.edu>
2025-02-10 12:39:38 -0800

    some notes for max

--- main.py
+++ main.py
@@ -2,11 +2,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# Define iniital parameters
-# Initial A, B, P, variances, tmax
-# Define ICs
+# Define initial parameters
+# Initial A, B, P, POPSHIFT, variances, tmax
+#IMPORTANT: 
+    #I added POPSHIFT = a walking variable that determines 
+    #how much of a virus's population will shift to the new variant 
+    #in the event that a new variant is created. this solves the problem
+    #of determining the initial population of a new variant
+    
+    #NOTES: this could be better implemented as a (possibly stochastic) function related to A
+    #for tuesday, maybe we just say POPSHIFT = 2A
+
+
+vir = [ #virus parameters
+    [0.3,0.2,0.1,0.2] #init virus has (A,B,P,POPSHIFT) = (0.3,0.2,0.1,0.2)
+]
+#variances = ?
+tmax = 300
+
+
+#
+# Define ICs. aka ystart
+ystart = (0.99,0.1,0) # init pop is (0.99,0.1,0) in S,I,R, respectively
+
 
 # Run Simulation
+
+#what we need is the following:
+
+'''
+and then we're done! if we reach t 200 and there is still at least one variant running, then we're good!
+'''
+
+
+
 
 # Plot evolution of A,B,P in t
 # Plot SIR model itself with total infected
--- sir_MEvariants.py
+++ sir_MEvariants.py
@@ -1,1 +1,14 @@
 # code to run entire simulation
+'''
+each timestep
+for all viruses (vir(n)):
+    walk a,b,p,POPSHIFT. 
+        IMPORTANT: this walking is not modifying vir(n). 
+            it is only used as input data for the formation of vir(n+1) if vir(n+1) is indeed created. (see next line)
+    if p hits
+        insert new virus at n+1
+        use the walked a,b,p,POPSHIFT to create a new variant at vir(n+1)
+        determine % of population present in I(n+1), subtract that from I(n)
+     use whatever our method is to favor more infectious & less deadly diseases 
+        IMPORTANT: we must figure how to do this in a way that is NOT just a hardcode
+'''
