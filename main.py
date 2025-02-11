adceb2477494732a7d4a0fccec96f229722cf34f
MCVLIX <mcvlix@eduroam-169-233-211-68.ucsc.edu>
2025-02-10 16:42:11 -0800

    SIR model with I as array - first commit

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+
+from sir_MEvariants import odeMutations
 
 # Define initial parameters
 # Initial A, B, P, variances, tmax
-#IMPORTANT: 
-    #I added POPSHIFT = a walking variable that determines 
-    #how much of a virus's population will shift to the new variant 
-    #in the event that a new variant is created. this solves the problem
-    #of determining the initial population of a new variant
-    
-    #NOTES: this could be better implemented as a (possibly stochastic) function related to A
-    #for tuesday, maybe we just say POPSHIFT = 2A
 
+virP = [[0.3,0.2,0.1],[0.3,0.2,0.1]] # [beta,gamma,pMutation] - initial virus parameters
+virV = [0.05, 0.05, 0.05] # = [vbeta,vgamma,vpMutation]
+tMax = 20
 
-vir = [ #virus parameters
-    [0.3,0.2,0.1] #init virus has (A,B,P) = (0.3,0.2,0.1)
-]
-#variances = ?
-tmax = 300
-
-
-#
 # Define ICs. aka ystart
-ystart = (0.99,0.1,0) # init pop is (0.99,0.1,0) in S,I,R, respectively
-
+ystart = [0.98,[0.01, 0.01],0] # init pop is (0.99,0.1,0) in S,I,R, respectively
 
 # Run Simulation
 
+y = odeMutations(virP, virV, tMax, ystart)
+
+# variants = []
+# for t in range(y.shape[0]):
+#     variants.append(y[t][1:-1])
+
+# print(variants)
+
 #what we need is the following:
-
-'''
-and then we're done! if we reach t 200 and there is still at least one variant running, then we're good!
-'''
-
-
-
 
 # Plot evolution of A,B,P in t
 # Plot SIR model itself with total infected
-# Plot SIR model with individual infected
+# Plot SIR model with individual infecte
+
+print(y)
+
+S = [t[0] for t in y]
+R = [t[2] for t in y]
+
+plt.plot(S, label="Susceptible")
+plt.plot(R, label="Recovered")
+
+# for i in range(0,len(y[-1][1])):
+#     V = [t[1][i] for t in y]
+#     print(V)
+#     plt.plot(V, label= f"Variant {i}")
+
+plt.legend()
+plt.show()
--- sir_MEvariants.py
+++ sir_MEvariants.py
@@ -1,4 +1,6 @@
 # code to run entire simulation
+import numpy as np
+
 '''
 each timestep
 for all viruses (vir(n)):
@@ -12,3 +14,60 @@ for all viruses (vir(n)):
      use whatever our method is to favor more infectious & less deadly diseases 
         IMPORTANT: we must figure how to do this in a way that is NOT just a hardcode
 '''
+
+def discreteMutation(virP, virV, y, t):
+
+    nV = y.shape[1]-2
+    pM = np.random.rand(nV)
+    
+    # loop over all I
+    for i in range(0, nV):
+        # run classical probability
+        if pM[i] <= virP[i-1][2]:
+
+            mutationIndex = len(y[1]) - 2
+
+            print(f'Timestep {t}: variant {i} has mutated to variant {mutationIndex}.')
+
+            # UPON CREATION OF NEW VIRUS: create new parameters virP
+                # modify current virP based off of virus
+                # add new virP to existing matrix
+
+            # create new I at y[t][i-2]
+        
+
+
+
+def odeMutations(virP, virV, tmax, ystart):
+
+    # simple SIR model ODE solver
+    y = [ystart]
+
+    for t in range(tmax):
+        
+        # Run mutation before integrating
+        # discreteMutation(virP,virV,y,t)   
+
+        # S,I,R values and parameters
+        # S and R stay as individual values
+        # I is instead compartmentalized into an array where p(Variant v) = I[v]
+        S = y[t][0]
+        I = y[t][1]
+        R = y[t][2]   
+
+        # iterate over each disease
+        for v in range(len(I)):
+
+            # I[v]= y[t][v]
+
+            beta = virP[v][0] # transmission
+            gamma = virP[v][1] # recovery
+            pMutation = virP[v][2] # mutation probab ility 
+
+            S += - beta * S * I[v]
+            I[v] += (beta * S * I[v]) - (gamma * I[v])
+            R += gamma * I[v]
+            
+        y.append([S,I,R])
+
+    return y
